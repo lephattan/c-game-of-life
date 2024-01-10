f2ae0940e96b47778b717d729160c9fdabab92c3
Tan Le <tanle2207@gmail.com>
2024-01-10 11:44:46 +0700

    dev: game land on GAMEPLAY screen first

--- src/cgameoflife.c
+++ src/cgameoflife.c
@@ -72,8 +72,9 @@ int main(void)
     // PlayMusicStream(music);
 
     // Setup and init first screen
-    currentScreen = LOGO;
-    InitLogoScreen();
+    // NOTE: land on GAMEPLAY screen directly for development speed
+    currentScreen = GAMEPLAY;
+    // InitLogoScreen();
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
